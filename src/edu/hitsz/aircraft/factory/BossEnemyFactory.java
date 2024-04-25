f9501b8697cb67bb38ca9f414037b0067fc633a9
xrlexpert <hiroxrl@outlook.com>
2024-04-25 10:30:52 +0800

    update

--- src/edu/hitsz/aircraft/factory/BossEnemyFactory.java
+++ src/edu/hitsz/aircraft/factory/BossEnemyFactory.java
@@ -5,13 +5,21 @@ import edu.hitsz.aircraft.BossEnemy;
 import edu.hitsz.application.ImageManager;
 import edu.hitsz.application.Main;
 
+import java.util.Random;
+
 public class BossEnemyFactory implements EnemyAircraftFactory{
     @Override
     public AbstractEnemyAircraft createAircraft() {
-        return new BossEnemy((int) (Math.random() * (Main.WINDOW_WIDTH - ImageManager.MOB_ENEMY_IMAGE.getWidth())),
-                (int) (Math.random() * Main.WINDOW_HEIGHT * 0.07),
-                4,
+        Random rand = new Random();
+        int randomNum = rand.nextInt(2);
+        int f = 1;
+        if(randomNum == 1){
+            f = -1;
+        }
+        return new BossEnemy((int) (Math.random() *( Main.WINDOW_WIDTH - ImageManager.MOB_ENEMY_IMAGE.getWidth())),
+                (int) (Main.WINDOW_HEIGHT * 0.15),
+                f*2,
                 0,
-                600);
+                450);
     }
 }
--- src/edu/hitsz/aircraft/factory/ElitePlusEnemyFactory.java
+++ src/edu/hitsz/aircraft/factory/ElitePlusEnemyFactory.java
@@ -6,12 +6,20 @@ import edu.hitsz.aircraft.ElitePlusEnemy;
 import edu.hitsz.application.ImageManager;
 import edu.hitsz.application.Main;
 
+import java.util.Random;
+
 public class ElitePlusEnemyFactory implements EnemyAircraftFactory  {
     @Override
     public AbstractEnemyAircraft createAircraft() {
+        Random rand = new Random();
+        int randomNum = rand.nextInt(2);
+        int f = 1;
+        if(randomNum == 1){
+            f = -1;
+        }
         return new ElitePlusEnemy((int) (Math.random() * (Main.WINDOW_WIDTH - ImageManager.MOB_ENEMY_IMAGE.getWidth())),
                 (int) (Math.random() * Main.WINDOW_HEIGHT * 0.05),
-                5,
+                f*2,
                 4,
                 30);
     }
